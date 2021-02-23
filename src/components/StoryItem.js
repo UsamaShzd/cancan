6d4e21b4c029280e1c3d3a32fcf279fcaaea9c7a
Usama Shehzad <usamashzd99@gmail.com>
2021-02-23 17:49:51 +0500

    story list implemented on home screen

--- src/components/StoryItem.js
+++ src/components/StoryItem.js
@@ -1,8 +1,40 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Image, StyleSheet } from "react-native";
+import AppText from "./common/AppText";
 
-const StoryItem = (props) => {
-  return <View />;
+const StoryItem = ({ imageUrl, text, containerStyle }) => {
+  return (
+    <View style={[styles.container, containerStyle]}>
+      <View style={styles.imageContainer}>
+        <Image
+          source={{ uri: imageUrl }}
+          resizeMode="cover"
+          style={styles.image}
+        />
+      </View>
+      <AppText style={styles.text}>{text}</AppText>
+    </View>
+  );
 };
 
 export default StoryItem;
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+  },
+  imageContainer: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    overflow: "hidden",
+  },
+  image: {
+    width: 50,
+    height: 50,
+  },
+
+  text: {
+    marginTop: 7,
+  },
+});
--- src/screens/ExploreScreen.js
+++ src/screens/ExploreScreen.js
@@ -1,12 +1,50 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, FlatList, StyleSheet } from "react-native";
+
 import Navbar from "../components/common/Navbar";
+import StoryItem from "../components/StoryItem";
+import colors from "../configs/colors";
 
 const ExploreScreen = ({ navigation }) => {
+  const [storyItems, setStoryItems] = useState([
+    { imageUrl: "https://picsum.photos/50", text: "Item 1" },
+    { imageUrl: "https://picsum.photos/50", text: "Item 2" },
+    { imageUrl: "https://picsum.photos/50", text: "Item 3" },
+    { imageUrl: "https://picsum.photos/50", text: "Item 4" },
+    { imageUrl: "https://picsum.photos/50", text: "Item 5" },
+    { imageUrl: "https://picsum.photos/50", text: "Item 6" },
+    { imageUrl: "https://picsum.photos/50", text: "Item 7" },
+  ]);
+  const renderStoryList = () => {
+    return (
+      <View style={styles.storyContainer}>
+        <FlatList
+          horizontal
+          data={storyItems}
+          showsHorizontalScrollIndicator={false}
+          showsVerticalScrollIndicator={false}
+          renderItem={({ item, index }) => {
+            return (
+              <StoryItem
+                imageUrl={item.imageUrl}
+                text={item.text}
+                containerStyle={{
+                  marginRight: 15,
+                  marginLeft: index === 0 ? 15 : 0,
+                }}
+              />
+            );
+          }}
+          keyExtractor={(item) => item.text}
+        />
+      </View>
+    );
+  };
   return (
     <View style={styles.container}>
       <Navbar title="Home" navigation={navigation} />
-      <Text>Explore Screen</Text>
+
+      {renderStoryList()}
     </View>
   );
 };
@@ -16,5 +54,10 @@ export default ExploreScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: colors.WHITE,
+  },
+
+  storyContainer: {
+    marginTop: 5,
   },
 });
