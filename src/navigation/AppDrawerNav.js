d00d6fb694c94e8a762222683fc44222e0e136ef
Usama Shehzad <usamashzd99@gmail.com>
2021-03-01 14:15:28 +0500

    drawer customized

--- src/navigation/AppDrawerNav.js
+++ src/navigation/AppDrawerNav.js
@@ -2,14 +2,45 @@ import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import CustomDrawer from "./CustomDrawer";
 
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import AppTabNav from "./AppTabNav";
+
+import colors from "../configs/colors";
 
 const { Navigator, Screen } = createDrawerNavigator();
 
 const AppDrawerNav = () => {
   return (
-    <Navigator drawerContent={CustomDrawer}>
-      <Screen name="AppTabNav" component={AppTabNav} />
+    <Navigator
+      drawerContent={CustomDrawer}
+      drawerContentOptions={{
+        activeTintColor: colors.PRIMARY_GREEN,
+
+        activeBackgroundColor: colors.WHITE,
+        itemStyle: {
+          borderBottomColor: colors.LIGHT_GREY,
+          borderBottomWidth: 1,
+          borderRadius: 0,
+          width: "100%",
+          marginLeft: 0,
+          paddingLeft: 10,
+        },
+      }}
+    >
+      <Screen
+        name="AppTabNav"
+        component={AppTabNav}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="storefront-outline"
+              color={color}
+              size={25}
+            />
+          ),
+          drawerLabel: "HOME",
+        }}
+      />
     </Navigator>
   );
 };
--- src/navigation/CustomDrawer.js
+++ src/navigation/CustomDrawer.js
@@ -1,15 +1,32 @@
 import React from "react";
+import { View, ScrollView, SafeAreaView, StyleSheet } from "react-native";
 import {
   DrawerContentScrollView,
   DrawerItemList,
 } from "@react-navigation/drawer";
 
+import colors from "../configs/colors";
+
 const CustomDrawer = (props) => {
   return (
-    <DrawerContentScrollView {...props}>
-      <DrawerItemList {...props} />
-    </DrawerContentScrollView>
+    <View style={{ flex: 1 }}>
+      <ScrollView style={{ flex: 1 }}>
+        <View style={styles.header} />
+        {/* <DrawerContentScrollView {...props}>
+         */}
+        <DrawerItemList {...props} />
+        {/* </DrawerContentScrollView> */}
+      </ScrollView>
+    </View>
   );
 };
 
 export default CustomDrawer;
+
+const styles = StyleSheet.create({
+  header: {
+    width: "100%",
+    height: 150,
+    backgroundColor: colors.PRIMARY_GREEN,
+  },
+});
